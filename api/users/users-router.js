d052c0282c9ae38b245eff0f2528ddd3c09a3a89
Ethan Edmond <eedmond@uccs.edu>
2021-07-07 13:35:57 -0600

    Wired middleware to endpoints

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -31,7 +31,7 @@ router.get('/:id', validateUserId, (req, res) => {
   });
 });
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
   res.status(201).json({
@@ -40,7 +40,7 @@ router.post('/', (req, res) => {
   });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
@@ -50,7 +50,7 @@ router.put('/:id', (req, res) => {
   });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
   res.status(200).json({
@@ -58,7 +58,7 @@ router.delete('/:id', (req, res) => {
   });
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
   res.status(200).json({
@@ -67,7 +67,7 @@ router.get('/:id/posts', (req, res) => {
   });
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
